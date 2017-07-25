134a7be5feef74f74816dd1d77735103f9681f81
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2017-07-25 13:57:42 -0700

    Added a property bag in BlazorContext

--- src/Blazor.Runtime/Components/BlazorContext.cs
+++ src/Blazor.Runtime/Components/BlazorContext.cs
@@ -1,15 +1,18 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
 using System.Security.Principal;
 
 namespace Blazor.Runtime.Components
 {
     public class BlazorContext
     {
-        public string AbsoluteUrl { get; set; }
-
         public BlazorContext(string absoluteUrl)
         {
             AbsoluteUrl = absoluteUrl;
         }
+
+        public string AbsoluteUrl { get; set; }
+
+        public IDictionary<string, object> Items { get; } = new Dictionary<string, object>();
     }
 }
