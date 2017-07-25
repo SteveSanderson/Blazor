23d3c4ad902ef86fd12bc0769a8c53d06edcf956
Justin Kotalik <t-jukota@microsoft.com>
2017-07-24 19:06:43 -0700

    Fixes sample

--- samples/ClientServerApp/ClientServerApp.Shared/Hello.cs
+++ samples/ClientServerApp/ClientServerApp.Shared/Hello.cs
@@ -6,6 +6,6 @@ namespace ClientServerApp.Shared
 {
     public class Hello
     {
-        public int MyProperty { get { return 500; } set { } }
+        public int MyProperty { get; set; }
     }
 }
