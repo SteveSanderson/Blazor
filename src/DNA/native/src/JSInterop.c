SteveSanderson/Blazor

commit 04296381728cdf4225aa5fbd0fd1945b1e89f78c

Enable debugging dna with VS

src/DNA/native/src/JSInterop.c
@@ -42,7 +42,7 @@ int JSInterop_CallDotNet(char* assemblyName, char* namespace, char* className, c
 
 tAsyncCall* Framework_JSInterop_ToHeapRef(PTR pThis_, PTR pParams, PTR pReturnValue) {
 	HEAP_PTR obj = ((HEAP_PTR*)pParams)[0];
-	*(U32*)pReturnValue = obj;
+	*(U32*)pReturnValue = *(U32*)obj;
 	return NULL;
 }
 

src/DNA/native/src/System.Type.c
@@ -180,7 +180,7 @@ tAsyncCall* System_Type_GetMethod(PTR pThis_, PTR pParams, PTR pReturnValue)
 			// Assign method def
 			pMethodInfo->methodBase.methodDef = pMethodDef;
 
-			*(HEAP_PTR*)pReturnValue = (HEAP_PTR*)pMethodInfo;
+			*(HEAP_PTR*)pReturnValue = *(HEAP_PTR*)pMethodInfo;
 			return NULL;
 		}
 	}

src/DNA/.gitignore
@@ -1,0 +1,3 @@
+Debug
+native/Debug
+native/x64

src/DNA/dna.sln
@@ -1,0 +1,28 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 14
+VisualStudioVersion = 14.0.25420.1
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "dna", "native\dna.vcxproj", "{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Debug|x64.ActiveCfg = Debug|x64
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Debug|x64.Build.0 = Debug|x64
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Debug|x86.ActiveCfg = Debug|Win32
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Debug|x86.Build.0 = Debug|Win32
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Release|x64.ActiveCfg = Release|x64
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Release|x64.Build.0 = Release|x64
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Release|x86.ActiveCfg = Release|Win32
+		{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal

src/DNA/native/dna.vcxproj
@@ -1,0 +1,244 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup Label="ProjectConfigurations">
+    <ProjectConfiguration Include="Debug|Win32">
+      <Configuration>Debug</Configuration>
+      <Platform>Win32</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Release|Win32">
+      <Configuration>Release</Configuration>
+      <Platform>Win32</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Debug|x64">
+      <Configuration>Debug</Configuration>
+      <Platform>x64</Platform>
+    </ProjectConfiguration>
+    <ProjectConfiguration Include="Release|x64">
+      <Configuration>Release</Configuration>
+      <Platform>x64</Platform>
+    </ProjectConfiguration>
+  </ItemGroup>
+  <ItemGroup>
+    <ClInclude Include="src\CIL_OpCodes.h" />
+    <ClInclude Include="src\CLIFile.h" />
+    <ClInclude Include="src\Compat.h" />
+    <ClInclude Include="src\Config.h" />
+    <ClInclude Include="src\Delegate.h" />
+    <ClInclude Include="src\EvalStack.h" />
+    <ClInclude Include="src\Finalizer.h" />
+    <ClInclude Include="src\Generics.h" />
+    <ClInclude Include="src\Heap.h" />
+    <ClInclude Include="src\InternalCall.h" />
+    <ClInclude Include="src\JIT.h" />
+    <ClInclude Include="src\JIT_OpCodes.h" />
+    <ClInclude Include="src\JSInterop.h" />
+    <ClInclude Include="src\MetaData.h" />
+    <ClInclude Include="src\MetaDataTables.h" />
+    <ClInclude Include="src\MethodState.h" />
+    <ClInclude Include="src\PInvoke.h" />
+    <ClInclude Include="src\PInvoke_CaseCode.h" />
+    <ClInclude Include="src\PInvoke_TypeDef.h" />
+    <ClInclude Include="src\RVA.h" />
+    <ClInclude Include="src\Sys.h" />
+    <ClInclude Include="src\System.Array.h" />
+    <ClInclude Include="src\System.Char.CaseConversion.h" />
+    <ClInclude Include="src\System.Char.h" />
+    <ClInclude Include="src\System.Char.UC_IndexRuns.h" />
+    <ClInclude Include="src\System.Console.h" />
+    <ClInclude Include="src\System.DateTime.h" />
+    <ClInclude Include="src\System.Diagnostics.Debugger.h" />
+    <ClInclude Include="src\System.Enum.h" />
+    <ClInclude Include="src\System.Environment.h" />
+    <ClInclude Include="src\System.GC.h" />
+    <ClInclude Include="src\System.IO.FileInternal.h" />
+    <ClInclude Include="src\System.Math.h" />
+    <ClInclude Include="src\System.Net.Dns.h" />
+    <ClInclude Include="src\System.Net.Sockets.Socket.h" />
+    <ClInclude Include="src\System.Object.h" />
+    <ClInclude Include="src\System.Reflection.MethodBase.h" />
+    <ClInclude Include="src\System.Reflection.MethodInfo.h" />
+    <ClInclude Include="src\System.Reflection.PropertyInfo.h" />
+    <ClInclude Include="src\System.Runtime.CompilerServices.RuntimeHelpers.h" />
+    <ClInclude Include="src\System.RuntimeType.h" />
+    <ClInclude Include="src\System.String.h" />
+    <ClInclude Include="src\System.Threading.Interlocked.h" />
+    <ClInclude Include="src\System.Threading.Monitor.h" />
+    <ClInclude Include="src\System.Threading.Thread.h" />
+    <ClInclude Include="src\System.Type.h" />
+    <ClInclude Include="src\System.ValueType.h" />
+    <ClInclude Include="src\System.WeakReference.h" />
+    <ClInclude Include="src\Thread.h" />
+    <ClInclude Include="src\Type.h" />
+    <ClInclude Include="src\Types.h" />
+  </ItemGroup>
+  <ItemGroup>
+    <ClCompile Include="src\CLIFile.c" />
+    <ClCompile Include="src\Delegate.c" />
+    <ClCompile Include="src\dna.c" />
+    <ClCompile Include="src\Finalizer.c" />
+    <ClCompile Include="src\Generics.c" />
+    <ClCompile Include="src\Heap.c" />
+    <ClCompile Include="src\InternalCall.c" />
+    <ClCompile Include="src\JIT.c" />
+    <ClCompile Include="src\JIT_Execute.c" />
+    <ClCompile Include="src\JSInterop.c" />
+    <ClCompile Include="src\MetaData.c" />
+    <ClCompile Include="src\MetaData_Fill.c" />
+    <ClCompile Include="src\MetaData_Search.c" />
+    <ClCompile Include="src\MethodState.c" />
+    <ClCompile Include="src\PInvoke.c" />
+    <ClCompile Include="src\RVA.c" />
+    <ClCompile Include="src\Sys.c" />
+    <ClCompile Include="src\System.Array.c" />
+    <ClCompile Include="src\System.Char.c" />
+    <ClCompile Include="src\System.Console.c" />
+    <ClCompile Include="src\System.DateTime.c" />
+    <ClCompile Include="src\System.Diagnostics.Debugger.c" />
+    <ClCompile Include="src\System.Enum.c" />
+    <ClCompile Include="src\System.Environment.c" />
+    <ClCompile Include="src\System.GC.c" />
+    <ClCompile Include="src\System.IO.FileInternal.c" />
+    <ClCompile Include="src\System.Math.c" />
+    <ClCompile Include="src\System.Net.Dns.c" />
+    <ClCompile Include="src\System.Net.Sockets.Socket.c" />
+    <ClCompile Include="src\System.Object.c" />
+    <ClCompile Include="src\System.Runtime.CompilerServices.RuntimeHelpers.c" />
+    <ClCompile Include="src\System.RuntimeType.c" />
+    <ClCompile Include="src\System.String.c" />
+    <ClCompile Include="src\System.Threading.Interlocked.c" />
+    <ClCompile Include="src\System.Threading.Monitor.c" />
+    <ClCompile Include="src\System.Threading.Thread.c" />
+    <ClCompile Include="src\System.Type.c" />
+    <ClCompile Include="src\System.ValueType.c" />
+    <ClCompile Include="src\System.WeakReference.c" />
+    <ClCompile Include="src\Thread.c" />
+    <ClCompile Include="src\Type.c" />
+  </ItemGroup>
+  <PropertyGroup Label="Globals">
+    <ProjectGuid>{F7FFFA55-C1B3-4961-8DAB-C1527EF2FF22}</ProjectGuid>
+    <Keyword>Win32Proj</Keyword>
+    <RootNamespace>dna</RootNamespace>
+    <WindowsTargetPlatformVersion>10.0.15063.0</WindowsTargetPlatformVersion>
+  </PropertyGroup>
+  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.Default.props" />
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'" Label="Configuration">
+    <ConfigurationType>Application</ConfigurationType>
+    <UseDebugLibraries>true</UseDebugLibraries>
+    <PlatformToolset>v141</PlatformToolset>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="Configuration">
+    <ConfigurationType>Application</ConfigurationType>
+    <UseDebugLibraries>false</UseDebugLibraries>
+    <PlatformToolset>v141</PlatformToolset>
+    <WholeProgramOptimization>true</WholeProgramOptimization>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'" Label="Configuration">
+    <ConfigurationType>Application</ConfigurationType>
+    <UseDebugLibraries>true</UseDebugLibraries>
+    <PlatformToolset>v141</PlatformToolset>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'" Label="Configuration">
+    <ConfigurationType>Application</ConfigurationType>
+    <UseDebugLibraries>false</UseDebugLibraries>
+    <PlatformToolset>v141</PlatformToolset>
+    <WholeProgramOptimization>true</WholeProgramOptimization>
+    <CharacterSet>Unicode</CharacterSet>
+  </PropertyGroup>
+  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />
+  <ImportGroup Label="ExtensionSettings">
+  </ImportGroup>
+  <ImportGroup Label="Shared">
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
+  </ImportGroup>
+  <PropertyGroup Label="UserMacros" />
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <LinkIncremental>true</LinkIncremental>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <LinkIncremental>true</LinkIncremental>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <LinkIncremental>false</LinkIncremental>
+  </PropertyGroup>
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <LinkIncremental>false</LinkIncremental>
+  </PropertyGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <ClCompile>
+      <PrecompiledHeader>
+      </PrecompiledHeader>
+      <WarningLevel>Level3</WarningLevel>
+      <Optimization>Disabled</Optimization>
+      <PreprocessorDefinitions>WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+    </ClCompile>
+    <Link>
+      <SubSystem>Console</SubSystem>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <AdditionalDependencies>ws2_32.lib;kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib;%(AdditionalDependencies)</AdditionalDependencies>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <ClCompile>
+      <PrecompiledHeader>
+      </PrecompiledHeader>
+      <WarningLevel>Level3</WarningLevel>
+      <Optimization>Disabled</Optimization>
+      <PreprocessorDefinitions>_DEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+    </ClCompile>
+    <Link>
+      <SubSystem>Console</SubSystem>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <ClCompile>
+      <WarningLevel>Level3</WarningLevel>
+      <PrecompiledHeader>
+      </PrecompiledHeader>
+      <Optimization>MaxSpeed</Optimization>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <PreprocessorDefinitions>WIN32;NDEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+    </ClCompile>
+    <Link>
+      <SubSystem>Console</SubSystem>
+      <EnableCOMDATFolding>true</EnableCOMDATFolding>
+      <OptimizeReferences>true</OptimizeReferences>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+    </Link>
+  </ItemDefinitionGroup>
+  <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <ClCompile>
+      <WarningLevel>Level3</WarningLevel>
+      <PrecompiledHeader>
+      </PrecompiledHeader>
+      <Optimization>MaxSpeed</Optimization>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <PreprocessorDefinitions>NDEBUG;_CONSOLE;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+    </ClCompile>
+    <Link>
+      <SubSystem>Console</SubSystem>
+      <EnableCOMDATFolding>true</EnableCOMDATFolding>
+      <OptimizeReferences>true</OptimizeReferences>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+    </Link>
+  </ItemDefinitionGroup>
+  <Import Project="$(VCTargetsPath)\Microsoft.Cpp.targets" />
+  <ImportGroup Label="ExtensionTargets">
+  </ImportGroup>
+</Project>

src/DNA/native/dna.vcxproj.filters
@@ -1,0 +1,297 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <ItemGroup>
+    <Filter Include="Source Files">
+      <UniqueIdentifier>{4FC737F1-C7A5-4376-A066-2A32D752A2FF}</UniqueIdentifier>
+      <Extensions>cpp;c;cc;cxx;def;odl;idl;hpj;bat;asm;asmx</Extensions>
+    </Filter>
+    <Filter Include="Header Files">
+      <UniqueIdentifier>{93995380-89BD-4b04-88EB-625FBE52EBFB}</UniqueIdentifier>
+      <Extensions>h;hh;hpp;hxx;hm;inl;inc;xsd</Extensions>
+    </Filter>
+    <Filter Include="Resource Files">
+      <UniqueIdentifier>{67DA6AB6-F800-4c08-8B7A-83BB121AAD01}</UniqueIdentifier>
+      <Extensions>rc;ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe;resx;tiff;tif;png;wav;mfcribbon-ms</Extensions>
+    </Filter>
+  </ItemGroup>
+  <ItemGroup>
+    <ClInclude Include="src\CIL_OpCodes.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\CLIFile.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Compat.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Config.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Delegate.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\EvalStack.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Finalizer.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Generics.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Heap.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\InternalCall.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\JIT.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\JIT_OpCodes.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\JSInterop.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\MetaData.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\MetaDataTables.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\MethodState.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\PInvoke.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\PInvoke_CaseCode.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\PInvoke_TypeDef.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\RVA.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Sys.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Array.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Char.CaseConversion.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Char.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Char.UC_IndexRuns.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Console.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.DateTime.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Diagnostics.Debugger.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Enum.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Environment.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.GC.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.IO.FileInternal.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Math.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Net.Dns.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Net.Sockets.Socket.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Object.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Reflection.MethodBase.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Reflection.MethodInfo.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Reflection.PropertyInfo.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Runtime.CompilerServices.RuntimeHelpers.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.RuntimeType.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.String.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Threading.Interlocked.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Threading.Monitor.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Threading.Thread.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.Type.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.ValueType.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\System.WeakReference.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Thread.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Type.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="src\Types.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+  </ItemGroup>
+  <ItemGroup>
+    <ClCompile Include="src\CLIFile.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Delegate.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\dna.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Finalizer.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Generics.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Heap.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\InternalCall.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\JIT.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\JIT_Execute.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\JSInterop.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\MetaData.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\MetaData_Fill.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\MetaData_Search.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\MethodState.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\PInvoke.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\RVA.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Sys.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Array.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Char.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Console.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.DateTime.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Diagnostics.Debugger.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Enum.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Environment.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.GC.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.IO.FileInternal.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Math.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Net.Dns.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Net.Sockets.Socket.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Object.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Runtime.CompilerServices.RuntimeHelpers.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.RuntimeType.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.String.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Threading.Interlocked.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Threading.Monitor.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Threading.Thread.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.Type.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.ValueType.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\System.WeakReference.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Thread.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="src\Type.c">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+  </ItemGroup>
+</Project>

src/DNA/native/src/Heap.c
@@ -382,7 +382,7 @@ static void GarbageCollect() {
 				}
 				// Use pSync to point to next entry in this linked-list.
 				//(tHeapEntry*)(pNode->pSync) = pToDelete;
-				pNode->pSync = pToDelete;
+				pNode->pSync = (tSync*)pToDelete;
 				pToDelete = pNode;
 			}
 		}

src/DNA/native/src/JIT_Execute.c
@@ -1115,7 +1115,7 @@ JIT_INVOKE_SYSTEM_REFLECTION_METHODBASE_start:
 		pCurrentMethodState->pReflectionInvokeReturnType = pCallMethod->pReturnType;
 
 		// Get the 'this' pointer for the call and the params array
-		PTR invocationThis = *(tMethodBase**)(pCurEvalStack + sizeof(HEAP_PTR));
+		PTR invocationThis = (PTR)(*(tMethodBase**)(pCurEvalStack + sizeof(HEAP_PTR)));
 		HEAP_PTR invocationParamsArray = *(HEAP_PTR*)(pCurEvalStack + sizeof(HEAP_PTR) + sizeof(PTR));		
 
 		// Put the new 'this' on the stack
@@ -1127,7 +1127,7 @@ JIT_INVOKE_SYSTEM_REFLECTION_METHODBASE_start:
 			U32 invocationParamsArrayLength = SystemArray_GetLength(invocationParamsArray);
 			PTR invocationParamsArrayElements = SystemArray_GetElements(invocationParamsArray);
 			for (U32 paramIndex = 0; paramIndex < invocationParamsArrayLength; paramIndex++) {
-				HEAP_PTR currentParam = ((U32*)(invocationParamsArrayElements))[paramIndex];
+				HEAP_PTR currentParam = (HEAP_PTR)(((U32*)(invocationParamsArrayElements))[paramIndex]);
 				if (currentParam == NULL) {
 					PUSH_O(NULL);
 				} else {

commit d243f2bfea304cc4a90ad4692844dea9bfa9b367

Fix issues introduced into DNA code in 04296381728cdf4225aa5fbd0fd1945b1e89f78c

src/DNA/native/src/JSInterop.c
@@ -42,7 +42,7 @@ int JSInterop_CallDotNet(char* assemblyName, char* namespace, char* className, c
 
 tAsyncCall* Framework_JSInterop_ToHeapRef(PTR pThis_, PTR pParams, PTR pReturnValue) {
 	HEAP_PTR obj = ((HEAP_PTR*)pParams)[0];
-	*(U32*)pReturnValue = *(U32*)obj;
+	*(HEAP_PTR*)pReturnValue = obj;
 	return NULL;
 }
 

src/DNA/native/src/System.Type.c
@@ -185,7 +185,7 @@ tAsyncCall* System_Type_GetMethod(PTR pThis_, PTR pParams, PTR pReturnValue)
 			// Assign method def
 			pMethodInfo->methodBase.methodDef = pMethodDef;
 
-			*(HEAP_PTR*)pReturnValue = *(HEAP_PTR*)pMethodInfo;
+			*(HEAP_PTR*)pReturnValue = (HEAP_PTR)pMethodInfo;
 			return NULL;
 		}
 	}